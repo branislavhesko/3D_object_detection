e5fa1c36a0bdb08ffb51a85f6b91948d86d3bdbd
Branislav Hesko <branislav.hesko@protonmail.ch>
2020-10-07 22:46:50 +0200

    fix for points

--- loss/contrastive_loss.py
+++ loss/contrastive_loss.py
@@ -24,13 +24,6 @@ class PositiveContrastiveLoss(torch.nn.Module):
     def forward(self, features_gt, features_object):
         pass
 
-    def find_closest_points(self, coords_gt, coords_object):
-        distance = torch.sqrt(torch.pow(coords_gt.unsqueeze(0) - coords_object.unsqueeze(1), 2).sum(-1))
-        closest_points = torch.argmin(distance, dim=1)
-        picked_gts = torch.arange(coords_gt.shape[0])
-        correct_idx = torch.where(distance[picked_gts, closest_points] > self._config.limit_positive_distance)
-        return picked_gts[correct_idx], closest_points[correct_idx]
-
 
 class NegativeContrastiveLoss(torch.nn.Module):
     def __init__(self, config: FeatureConfig):
@@ -40,22 +33,28 @@ class NegativeContrastiveLoss(torch.nn.Module):
     def forward(self, features_model, features_pointcloud):
         pass
 
-    def find_furthest_points(self, coords_gt, coords_object):
-        distance = torch.sqrt(torch.pow(coords_gt.unsqueeze(0) - coords_object.unsqueeze(1), 2).sum(-1))
-        furthest_points = torch.argmax(distance, dim=1)
-        picked_gts = torch.arange(coords_gt.shape[0])
-        return picked_gts, furthest_points
 
-    def find_model_opposite_points(self, coords_gt, coords_object):
-        negative_points = np.random.choice(coords_gt.shape[0], self._config.num_negative_pairs)
-        distance = torch.sqrt(torch.pow(coords_gt.unsqueeze(0) - coords_gt.unsqueeze(1), 2).sum(-1))
-        random_distribution = torch.distributions.exponential.Exponential(rate=5.)
-        distance *= (1 - random_distribution.sample(sample_shape=distance.shape))
-        furthest_points = torch.argmax(distance, dim=1)
-        picked_gts = torch.arange(coords_gt.shape[0])
-        return picked_gts, furthest_points
+def find_negative_matches(coords_gt, coords_object, positive_distance_limit):
+    distance = torch.sqrt(torch.pow(coords_gt - coords_object, 2).sum(-1))
+    return coords_gt[distance > positive_distance_limit, ...], coords_object[distance > positive_distance_limit, ...]
+
+
+def find_model_opposite_points(coords_gt, coords_object, positive_distance_limit):
+    distance = torch.sqrt(torch.pow(coords_gt.unsqueeze(0) - coords_gt.unsqueeze(1), 2).sum(-1))
+    random_distribution = torch.distributions.exponential.Exponential(rate=5.)
+    distance *= (1 - random_distribution.sample(sample_shape=distance.shape))
+    furthest_points = torch.argmax(distance, dim=1)
+    return coords_gt, find_positive_matches(coords_gt[furthest_points], coords_object, positive_distance_limit)[1]
+
+
+def find_positive_matches(coords_gt, coords_object, positive_distance_limit):
+    distance = torch.sqrt(torch.pow(coords_gt.unsqueeze(0) - coords_object.unsqueeze(1), 2).sum(-1))
+    closest_points = torch.argmin(distance, dim=1)
+    picked_gts = torch.arange(coords_gt.shape[0])
+    correct_idx = torch.where((distance[picked_gts, closest_points] < positive_distance_limit))
+    return coords_gt[picked_gts[correct_idx]], coords_object[closest_points[correct_idx]]
 
 
 if __name__ == "__main__":
     neg = PositiveContrastiveLoss(FeatureConfig())
-    neg.find_closest_points(torch.randint(low=0, high=100, size=(100, 3)).float(), torch.randint(low=0, high=100, size=(100, 3)).float())
+    print(find_model_opposite_points(torch.randint(low=0, high=100, size=(100, 3)).float(), torch.randint(low=0, high=100, size=(100, 3)).float(), 100)[0].shape)
